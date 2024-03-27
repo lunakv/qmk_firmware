e471a4f4e77547be216e9c174a5d321851f02d25
Václav Luňák <v.lunak@gmail.com>
2024-03-27 22:08:58 +0100

    lunakv sofle layout

---
+++ keyboards/sofle/keymaps/lunakv/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#define EE_HANDS
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define TAPPING_TERM 200
+
